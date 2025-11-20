6b672a7b873b00f4519e340a43e7111694b19d78
Chidera Christian <chiderachristain36@gmail.com>
2025-11-20 08:20:42 +0100

    mobile

---
+++ src/firebase/station2.js
@@ -0,0 +1,31 @@
+// src/firebase/useFirebase.js
+import { useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2VTPo6HFQ95sNJqCLzT1kvS90hI8Javc",
+  authDomain: "earlyfloodalertsystem.firebaseapp.com",
+  databaseURL: "https://earlyfloodalertsystem-default-rtdb.firebaseio.com",
+  projectId: "earlyfloodalertsystem",
+  storageBucket: "earlyfloodalertsystem.firebasestorage.app",
+  messagingSenderId: "872515673613",
+  appId: "1:872515673613:web:d2ac5632517666308e7f44",
+  measurementId: "G-1JS6SKK3WE",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default function useFirebase(callback) {
+  useEffect(() => {
+    const db = getDatabase(app);
+    const dbRef = ref(db, "flood_alert/station2");
+
+    const unsubscribe = onValue(dbRef, (snapshot) => {
+      const data = snapshot.val();
+      callback(data);
+    });
+
+    return () => unsubscribe();
+  }, [callback]);
+}
